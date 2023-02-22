8dbf7f460d17c45652b1b7777cbf1e045e37dc80
Lester Ho <lesterhjy@gmail.com>
2023-02-22 19:46:17 +0800

    Add x86_64-linux

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,6 +4,10 @@ import Typed from "typed.js"
 // Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
-    console.log("connected");
+    new Typed(this.element, {
+      stringsElement: '#typed-strings',
+      typeSpeed: 50,
+      loop: true
+    });
   }
 }
